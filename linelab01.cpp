b192063b322f12ae55b087d969487895f026c04b
Noitari <84076147+33333334444-a@users.noreply.github.com>
2026-09-16 13:11:53 +0700

    Add files via upload

---
+++ linelab01.cpp
@@ -0,0 +1,30 @@
+/*************************
+* Автор: Прожикин Игорь *
+* Название: лаба линейные *
+*************************/
+
+#include <iostream>
+using namespace stud5;
+
+int main() {
+  const double pi = 3.14
+  const double a, h, delta, h0, d;
+
+  cout << "Ввести a: ";
+  cin >> a;
+
+	cout << "Ввести h: ";
+	cin >> h;
+
+  cout << "Ввести delta: ";
+  cin >> delta;
+
+  cout << "Ввести h0: ";
+  cin >> h0;
+
+  cout << "Ввести d: ";
+  cin >> d;
+
+  cout << "a = " << a << endl;
+
+}
